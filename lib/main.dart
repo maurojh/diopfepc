aad2a4f0bd94681a1d0d1f0759822962378544dd
maurojh <maurojh@gmail.com>
2023-09-12 06:42:18 -0300

    exemplo Container

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+// stl TAB 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+// stf TAB
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
