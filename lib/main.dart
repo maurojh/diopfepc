1836c6a7170ffb0139722ef7fdbcad6b9a9f7359
maurojh <maurojh@gmail.com>
2023-09-13 07:01:41 -0300

    chamando build

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    debugPrint('chamando build');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meu App'),
